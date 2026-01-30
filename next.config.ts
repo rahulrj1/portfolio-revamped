0daf04106cff40b1d79e6540896cd951f67420b2
Rahul Kumar <rahulrj1@gmail.com>
2026-01-31 05:06:32 +0530

    changes for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  // If deploying to a GitHub Project Page (e.g. username.github.io/repo-name),
-  // uncomment the following line and replace '/repo-name' with your repository name:
-  // basePath: '/personal-portfolio',
+  basePath: '/portfolio-revamped',
+  assetPrefix: '/portfolio-revamped/',
 };
 
 export default nextConfig;
